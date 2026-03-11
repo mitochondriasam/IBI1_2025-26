c2cf0c0743c77c984b50f4b20244737c5ffd6f5b
mitochondriasam <01_ethanol.conchs@icloud.com>
2026-03-11 13:39:37 +0800

    Add initial implementation of Python scripts for practical exercises
    - Create .gitignore to exclude unnecessary files
    - Add creatine_clearance.py, infection.py, mystery_code.py, and variables.py with basic functionality

---
+++ Practical4/variables.py
@@ -0,0 +1,12 @@
+a = 5.08
+b = 5.33
+c = 5.55
+
+# claculate the difference between b and a, and c and b
+d = b - a
+e = c - b
+
+print(d, e)
+
+# compare d and e, and print which one is larger
+print("d is larger than e") if d > e else print("e is larger than d")
