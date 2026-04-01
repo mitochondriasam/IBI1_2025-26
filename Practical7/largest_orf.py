76ef1f653e69d3f59ccab0f986bc0b3914a13141
SAMurai <199494036+mitochondriasam@users.noreply.github.com>
2026-04-01 13:33:13 +0800

    Add largest ORF finder script

---
+++ Practical7/largest_orf.py
@@ -0,0 +1,17 @@
+seq = 'AAGAUACAUGCAAGUGGUGUGUCUGUUCUGAGAGGGCCUAAAAG'
+
+def find_orfs(seq):
+    orfs = []
+    for i in range(len(seq)-2):
+        codon = seq[i:i+3]
+        if codon == 'AUG':
+            for j in range(i+3, len(seq)-2, 3):
+                stop_codon = seq[j:j+3]
+                if stop_codon in ['UAA', 'UAG', 'UGA']:
+                    orfs.append(seq[i:j+3])
+                    break
+    return orfs
+
+orfs = find_orfs(seq)
+largest_orf = max(orfs, key=len) if orfs else None
+print(largest_orf)
